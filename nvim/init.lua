ce12827acef75c4c1612d2b45f7dacf8c072ae9a
John Watson <johnw@planetscale.com>
2026-07-10 15:14:39 -0700

    nvim/init.lua: migrate to new Treesitter setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -58,52 +58,29 @@ require("lazy").setup({
 	-- tree-sitter
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
 		build = ":TSUpdate",
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"bash",
-					"go",
-					"gomod",
-					"hcl",
-					"lua",
-					"markdown",
-					"markdown_inline",
-					"mermaid",
-					"ruby",
-					"vim",
-					"vimdoc",
-					"yaml",
-          "json5",
-				},
-				indent = {
-					enable = true,
-					disable = {},
-				},
-				incremental_selection = {
-					enable = true,
-				},
-				autopairs = {
-					enable = true,
-				},
-				highlight = {
-					enable = true,
+			require("nvim-treesitter").install({
+				"bash",
+				"go",
+				"gomod",
+				"hcl",
+				"json5",
+				"mermaid",
+				"ruby",
+				"yaml",
+			})
 
-					-- Disable slow treesitter highlight for large files
-					disable = function(lang, buf)
-						local max_filesize = 100 * 1024 -- 100 KB
-						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-						if ok and stats and stats.size > max_filesize then
-							return true
-						end
-					end,
+			-- Needed because Neovim's shell filetype is usually "sh"
+			vim.treesitter.language.register("bash", { "sh", "bash" })
 
-					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-					-- Using this option may slow down your editor, and you may see some duplicate highlights.
-					-- Instead of true it can also be a list of languages
-					additional_vim_regex_highlighting = false,
-				},
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "*",
+				callback = function(args)
+					pcall(vim.treesitter.start, args.buf)
+				end,
 			})
 		end,
 	},
